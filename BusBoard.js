59f1ea9e1f47c0eb54d357d58ec839554c813843
Alison Thomson <Alison.Thomson@softwire.com>
2022-01-20 11:58:21 +0000

    First commit

---
+++ BusBoard.js
@@ -0,0 +1,6 @@
+const fetch = import("node-fetch");
+fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
+    .then(response => response.json())
+    .then(body => console.log(body));
+
+    
---
+++ busboard.mjs
@@ -0,0 +1,14 @@
+import fetch from "node-fetch";
+
+fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
+    .then(response => response.json())
+    //.then(body => console.log(body))
+    .then(json => {
+        for (const dis of json) {
+            console.log("Stop: " + dis.stationName);
+            console.log("Route: " + dis.lineName);
+            console.log("Destination: " + dis.destinationName);
+            console.log("Arriving: " + dis.expectedArrival);
+        }
+    })
+    
