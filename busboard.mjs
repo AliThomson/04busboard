10d3c1e36a07f71993226226ca6f29fabdea9883
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-01-21 13:30:54 +0000

    check postcode done

--- busboard.mjs
+++ busboard.mjs
@@ -1,17 +1,30 @@
 import fetch from 'node-fetch';
 import readlineSync from 'readline-sync';
+import winston from 'winston'
+
+function isValidPostcode(p) { 
+    let postcodeRegEx = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/i; 
+    return postcodeRegEx.test(p); 
+}
+console.log(isValidPostcode("nw51tl"))
+
+// while(isValidPostcode(p) === false) {
+//     try {
+//         throw new Error ("Invalid Postcode. Please try again!")
+//     }
+//     catch(err) {
+//         console.error("Invalid Postcode. Please try again!")
+//     }
+// }
 
 let userPostcode = "";
 
 const askPostcode = () => {
     userPostcode = readlineSync.question("Please enter your post code: ")
-    console.log(userPostcode);
+    
 }
 askPostcode();
 
-
-// const response = await fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals");
-// const buses = await response.json();
 
 
 
@@ -22,51 +35,33 @@ const postCodeResponse = await responsePostcode.json();
 const longitude = postCodeResponse.result.longitude;
 const latitude = postCodeResponse.result.latitude;
 
-console.log(longitude, latitude);
 
-const responseRadius = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${latitude}&lon=${longitude}&stopTypes=NaptanPublicBusCoachTram&radius=150`);
+
+const responseRadius = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${latitude}&lon=${longitude}&stopTypes=NaptanPublicBusCoachTram&radius=1000`);
 const busStops = await responseRadius.json();
 
 const firstTwoStop = busStops.stopPoints.slice(0, 2)
-console.log(firstTwoStop);
 
-
-
-// const response = await fetch("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals");
-// const buses = await response.json();
 
 for (let stop of firstTwoStop) {
 
 
     const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stop.naptanId}/Arrivals`);
     const buses = await response.json();
+    
+    
 
    const sortedBuses = buses.sort((bus1, bus2) => bus1.timeToStation - bus2.timeToStation);
 
     const last5buses = sortedBuses.slice(0,5)
 
-    console.log(`Buses for stop no: ${stop.naptanId}`)
-     for(const bus of last5buses) {
     
-    console.log("Bus to " + bus.destinationName + " arrving in " + bus.timeToStation + "seconds");
+     for(const bus of last5buses) {
+
+    console.log(`[${bus.stationName}]: Bus to ${bus.destinationName} arrving in ${bus.timeToStation} seconds`);
  
      }
 
-
-
-
-
-   console.log(stop.naptanId);
 }
-// const sortedBuses = buses.sort((bus1, bus2) => bus1.timeToStation - bus2.timeToStation);
-
-// const last5buses = sortedBuses.slice(0,5)
-
-
-// for(const bus of last5buses) {
-    
-//     console.log("Bus to " + bus.destinationName + " arrving in " + bus.timeToStation + "seconds");
- 
-// }
 
 
